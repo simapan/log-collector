5257ba5760099ad51f692dbbd942264bd7a03077
simapan <1662113640@qq.com>
2021-05-10 22:47:19 +0800

    Initial commit

---
+++ src/main/java/com/sjj/appclient/AppMain.java
@@ -0,0 +1,7 @@
+package com.sjj.appclient;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/sjj/bean/Person.java
@@ -0,0 +1,10 @@
+package com.sjj.bean;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    private String name;
+    private String sex;
+    private  int age;
+    
+}
